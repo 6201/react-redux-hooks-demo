a81bad27c34adfb059be9570c5066b376f19de6b
yzc <rd_yzc@ely.work>
2020-03-24 17:38:39 +0800

    useCallback

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { useSelector, useDispatch, shallowEqual } from 'react-redux';
 import './App.css';
 
 function App() {
   const dispatch = useDispatch();
-
+  const increment = useCallback(
+    () => dispatch({ type: 'increment-counter' }),
+    [dispatch]
+  )
+  const diminished = useCallback(
+    () => dispatch({ type: 'diminished-counter' }),
+    [dispatch]
+  )
   const { count } = useSelector(state => state.count, shallowEqual);
   return (
     <div className="App">
       <div>
         <span>{count}</span>
       </div>
-      <button onClick={() => dispatch({ type: 'increment-counter' })}>+</button>
-      <button onClick={() => dispatch({ type: 'diminished-counter' })}>-</button>
+      <Button onClick={increment} content="+"/>
+      <Button onClick={diminished} content="-"/>
     </div>
   );
 }
+
+const Button =  React.memo(({onClick, content}) => (
+  <button onClick={onClick}>
+    {content}
+  </button>
+))
 
 export default App;
